ec9fd24970ba25563c0661ac1d7af85ff5e6ddf3
barbaracarvajal <barb.carvajalsaez@gmail.com>
2023-08-26 21:30:01 -0400

    todo done

---
+++ src/components/Tarea.tsx
@@ -0,0 +1,16 @@
+type Tarea = {
+    tarea: string
+    borrarTarea: () => void
+}
+
+const Tarea = ({tarea, borrarTarea}: Tarea) => {
+    return (
+        <div className="task">
+            <span>{tarea}</span>
+            <button onClick={borrarTarea}>Borrar</button>
+            
+        </div>
+    )
+}
+
+export default Tarea
